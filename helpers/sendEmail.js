b09adf7b65d7675459614f0ac2fb903963a1d49d
Erik Richard <erichard413@outlook.com>
2024-02-09 10:54:40 -0500

    changes to check in, fixed sendGrid

--- helpers/sendEmail.js
+++ helpers/sendEmail.js
@@ -1,28 +1,30 @@
 require("dotenv").config();
 
-const sgMail = require('@sendgrid/mail');
+const sgMail = require("@sendgrid/mail");
 sgMail.setApiKey(process.env.SENDGRID_API_KEY);
 
 function sendEmail(email, password) {
-    const msg = {
-        to: email,
-        from: 'erik@travelingdisc.com',
-        subject: 'Your password has been reset!',
-        text: `Hello, 
+  const msg = {
+    to: email,
+    from: "erik@travelingdisc.com",
+    subject: "Your password has been reset!",
+    text: `Hello, 
         
     It looks like you have forgotten your password, no worries! Your password has been reset to: ${password}
         
     Have a great day,
         
-    Erik`
-    }
-    sgMail.send(msg).then((response)=>{
-        console.log(response[0].statusCode)
-        console.log(response[0].headers)
+    Erik`,
+  };
+  sgMail
+    .send(msg)
+    .then(response => {
+      console.log(response[0].statusCode);
+      console.log(response[0].headers);
     })
-    .catch((err) => {
-        console.error(err);
-    })
+    .catch(err => {
+      console.error(err);
+    });
 }
 
-module.exports = {sendEmail};
+module.exports = { sendEmail };
